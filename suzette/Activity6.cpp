30542b66edb0db2b8c6c5eae5089c8a9cb7f1834
Suzette-jpg <suzettesacueza072003@gmail.com>
2024-10-07 21:30:57 +0800

    Create Activity6.cpp

---
+++ suzette/Activity6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+ using namespace std;
+
+  int main(){
+
+    string name = "Suzette";
+    char middle = 'I';
+    string last = "Sacueza";
+    int age = 21;
+    bool boolvalue = true;
+    double height = 152.40;
+    float weight = 73;
+    
+    cout << "Name: " << name << endl;
+    cout << "MiddleName: " << middle << endl;
+    cout << "LastName: " << last << endl;
+    cout << "Age:" << age << endl;
+    cout << "Male:" << boolvalue << endl;
+    cout << "Height(cm): " << height << endl;
+    cout << "Weight(kg): " << weight << endl;
+    
+    
+    return 0;
